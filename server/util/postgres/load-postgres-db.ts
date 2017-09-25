bad3c4c6d00fde336fae9dc1f3c67a9218e0ce24
John <john.reeme@gmail.com>
2017-09-25 14:19:55 -0500

    Add Influent models for OpenStack instance

--- server/util/postgres/load-postgres-db.ts
+++ server/util/postgres/load-postgres-db.ts
@@ -1,44 +1,62 @@
-var loopback = require('loopback');
-var ds = loopback.createDataSource('postgres', {
-  "host": "localhost",
-  "port": 5432,
-  "url": "postgres://northwind_user:thewindisblowing@localhost/northwind",
-  "database": "northwind",
-  "user": "northwind_user",
-  "connector": "postgresql",
-  "password": "thewindisblowing"
+const loopback = require('loopback');
+const fs = require('fs');
+const path = require('path');
+const modelConfigPath = path.resolve(__dirname, '../../model-config.json');
+const modelsPath = path.resolve(__dirname, '../../../common/models');
+const dataSource = loopback.createDataSource('mysql', {
+  //host: "127.0.0.1",
+  host: "10.1.70.196",
+  port: 3306,
+  url: "",
+  database: "aml",
+  name: "mysql",
+  user: "influent",
+  password: "influent",
+  acquireTimeout: 30000,
+  connectTimeout: 30000,
+  connector: "mysql"
 });
 
-ds.discoverModelDefinitions({views: false, limit: 100}, (err, tables) => {
+function convertModelNameToFileName(modelName) {
+  return modelName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
+
+function defaultJsFileContents(modelName) {
+  return 'module.exports = function(/*' + modelName + '*/){};'
+}
+
+function updateModelConfig(schema, dataSource) {
+  const cfg = JSON.parse(fs.readFileSync(modelConfigPath));
+
+  cfg[schema.name] = {
+    dataSource: dataSource.name,
+    'public': true
+  };
+
+  fs.writeFileSync(modelConfigPath, JSON.stringify(cfg, null, 2));
+}
+
+function writeFilesForModelSchema(schema) {
+  const filePath = modelsPath + '/' + convertModelNameToFileName(schema.name);
+  fs.writeFileSync(filePath + '.json', JSON.stringify(schema, null, 2));
+  fs.writeFileSync(filePath + '.js', defaultJsFileContents(schema.name));
+  updateModelConfig(schema, dataSource);
+}
+
+dataSource.discoverModelDefinitions({
+  views: true,
+  all: false,
+  owner: 'aml',
+  limit: 100
+}, (err, tables) => {
   tables.forEach((table) => {
-    let tableName = table.name;
-    //ds.discoverAndBuildModels(tableName, {visited: {}, associations: true},
-    /*  tableName,
-        {visited: {}, associations: true},*/
-    ds.discoverSchemas(
-      tableName,
-      {relations: true, all: true, views: true},
-      (err, models) => {
-        let e = err;
+    dataSource.discoverSchema(
+      table.name,
+      //{relations: true, all: true, views: true},
+      {owner: 'aml'},
+      (err, schema) => {
+        writeFilesForModelSchema(schema);
       });
   });
 });
-/*
-ds.discoverAndBuildModels('INVENTORY', {visited: {}, associations: true},
-function (err, models) {
-  // Now we have a list of models keyed by the model name
-  // Find the first record from the inventory
-  models.Inventory.findOne({}, function (err, inv) {
-    if(err) {
-      console.error(err);
-      return;
-    }
-    console.log("\nInventory: ", inv);
-    // Navigate to the product model
-    // Assumes inventory table has a foreign key relationship to product table
-    inv.product(function (err, prod) {
-      console.log("\nProduct: ", prod);
-      console.log("\n ------------- ");
-    });
-  });
-});*/
+
