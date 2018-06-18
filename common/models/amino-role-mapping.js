54001eec3ac41640ba8040e6d82b696bb9d09741
jstnldrs <justin.lueders@gmail.com>
2018-06-18 17:33:49 -0500

    name model correctly

--- common/models/amino-role-mapping.js
+++ common/models/amino-role-mapping.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = function(Aminorolemapping) {
+module.exports = function(AminoRoleMapping) {
 
 };
--- common/models/amino-role.js
+++ common/models/amino-role.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = function(Aminorole) {
+module.exports = function(AminoRole) {
 
 };
