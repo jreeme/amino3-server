71ea1c13d6e6e3b4c5c430c70687348ead930bec
John <john.reeme@gmail.com>
2018-06-29 11:47:42 -0500

    Take out '/amino3/' path and return <base href= to '/'> so Angular standalone running will work (and it's just cleaner)

--- server/startup/boot-manager.ts
+++ server/startup/boot-manager.ts
@@ -139,12 +139,12 @@ export class BootManagerImpl implements BootManager {
       },
       (cb) => {
         //Redirect root to amino3, magic string here (rather than on Globals) because it exists in middleware & HTML too
-        me.app.all('/explorer', (req, res) => {
+/*        me.app.all('/explorer', (req, res) => {
           res.redirect('/amino3/explorer');
         });
         me.app.all('/', (req, res) => {
           res.redirect('/amino3');
-        });
+        });*/
         cb();
       },
       (cb) => {
