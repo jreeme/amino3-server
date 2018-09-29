bc9096c61e0d09e11d0852f83145247445a92afb
jstnldrs <justin.lueders@gmail.com>
2018-09-28 21:26:13 -0500

    Adding /indices/verb endpoint for ES

--- common/models/elasticsearch.js
+++ common/models/elasticsearch.js
@@ -1,12 +1,12 @@
 module.exports = function (Elasticsearch) {
-  Elasticsearch.elasticsearchAction = function (esVerb, esQueryJson, req, res) {
+  Elasticsearch.elasticsearchVerb = function (esVerb, esQueryJson, req, res) {
     //Due to loopback-sdk-builder implementation "feature" we have to do route variable
     //substitution ourselves. Fortunately loopback-sdk-builder sends the substitutions
     //in the query string
     esVerb = (esVerb === ':esVerb') ? req.query.esVerb : esVerb;
     global.postal.publish({
       channel: 'Elasticsearch',
-      topic: 'Query',
+      topic: 'Verb',
       //See: server/services/elasticsearch/elasticsearch.ts + ElasticsearchQuery interface
       data: {
         esVerb,
@@ -16,49 +16,109 @@ module.exports = function (Elasticsearch) {
       }
     });
   };
-
-  const accepts = [
-    {
-      arg: 'esVerb',
-      type: 'string',
-      required: true,
-      description: 'Elasticsearch action to perform (_search, _count, etc.)'
-    },
-    {
-      arg: 'esQueryJson',
-      type: 'object',
-      http: {source: 'body'}
-    },
-    {
-      arg: 'req',
-      type: 'object',
-      http: {source: 'req'}
-    },
-    {
-      arg: 'res',
-      type: 'object',
-      http: {source: 'res'}
+  Elasticsearch.remoteMethod('elasticsearchVerb', {
+    accepts:[
+      {
+        arg: 'esVerb',
+        type: 'string',
+        required: true,
+        description: 'Elasticsearch action to perform (_search, _count, etc.)'
+      },
+      {
+        arg: 'esQueryJson',
+        type: 'object',
+        http: {source: 'body'}
+      },
+      {
+        arg: 'req',
+        type: 'object',
+        http: {source: 'req'}
+      },
+      {
+        arg: 'res',
+        type: 'object',
+        http: {source: 'res'}
+      }
+    ],
+    returns:[
+      {
+        arg: 'result',
+        type: 'object',
+        root: true,
+        description: 'Result of Elasticsearch query'
+      }
+    ],
+    http:{
+      path: '/es/:esVerb',
+      verb: 'all'
     }
-  ];
+  });
 
-  const returns = [
-    {
-      arg: 'result',
-      type: 'object',
-      root: true,
-      description: 'Result of Elasticsearch query'
-    }
-  ];
 
-  const http = {
-    path: '/es/:esVerb',
-    verb: 'all'
+  Elasticsearch.elasticsearchIndicesVerb = function (esIndices, esVerb, esQueryJson, req, res) {
+    //Due to loopback-sdk-builder implementation "feature" we have to do route variable
+    //substitution ourselves. Fortunately loopback-sdk-builder sends the substitutions
+    //in the query string
+    esIndices = (esIndices === ':esIndices') ? req.query.esIndices : esIndices;
+    esVerb = (esVerb === ':esVerb') ? req.query.esVerb : esVerb;
+    global.postal.publish({
+      channel: 'Elasticsearch',
+      topic: 'IndicesVerb',
+      //See: server/services/elasticsearch/elasticsearch.ts + ElasticsearchQuery interface
+      data: {
+        esIndices,
+        esVerb,
+        esQueryJson,
+        req,
+        res
+      }
+    });
   };
 
-  Elasticsearch.remoteMethod('elasticsearchAction', {
-    accepts,
-    returns,
-    http
+  Elasticsearch.remoteMethod('elasticsearchIndicesVerb', {
+    accepts:[
+      {
+        arg: 'esIndices',
+        type: 'string',
+        required: true,
+        description: 'Elasticsearch indices to perform the verb on (indice1,indice2)'
+      },
+      {
+        arg: 'esVerb',
+        type: 'string',
+        required: true,
+        description: 'Elasticsearch action to perform (_search, _count, etc.)'
+      },
+      {
+        arg: 'esQueryJson',
+        type: 'object',
+        http: {source: 'body'}
+      },
+      {
+        arg: 'req',
+        type: 'object',
+        http: {source: 'req'}
+      },
+      {
+        arg: 'res',
+        type: 'object',
+        http: {source: 'res'}
+      }
+    ],
+    returns:[
+      {
+        arg: 'result',
+        type: 'object',
+        root: true,
+        description: 'Result of Elasticsearch query'
+      }
+    ],
+    http:{
+      path: '/es/:esIndices/:esVerb',
+      verb: 'all'
+    }
   });
+
+
 };
 
