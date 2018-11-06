06ddedd94eab22fca9b37811db4427961396faf3
Robert <rvemble@gmail.com>
2018-11-06 12:46:55 -0600

    adding metadata catalog relationship tables (tags, pedigrees)

--- common/models/metadata-info-catalog.js
+++ common/models/metadata-info-catalog.js
@@ -4,7 +4,7 @@ module.exports = function(MetadataInfoCatalog) {
 
   MetadataInfoCatalog.remoteMethod('deleteAll', {
     isStatic: true,
-    description: 'Delete all instances based on where criteria from the data source',
+    description: 'Delete all instances from the data source based on where criteria',
     accessType: 'WRITE',
     accepts: {arg: 'where', type: 'object', description: 'Criteria to match model instances'},
     returns: [
